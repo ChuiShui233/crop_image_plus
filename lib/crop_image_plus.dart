38aa0e8d3aef27859a0fa6645ae6bc22cde57a42
吹水明月 <chaoscodes512@outlook.com>
2025-10-02 18:32:10 +0800

    PLUS

--- lib/crop_image_plus.dart
+++ lib/crop_image_plus.dart
@@ -1,3 +1,3 @@
-export 'package:crop_image/src/crop_controller.dart';
-export 'package:crop_image/src/crop_image.dart';
-export 'package:crop_image/src/crop_rotation.dart';
+export 'src/crop_controller.dart';
+export 'src/crop_image.dart';
+export 'src/crop_rotation.dart';
